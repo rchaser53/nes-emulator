8c5b6607e33b40bf00e32f5ffa0caf19054e76b5
rchaser53 <tayoshizawa29@gmail.com>
2018-03-21 00:19:50 +0900

    add memo for opecodes

---
+++ memo.md
@@ -0,0 +1,23 @@
+- Uint8Array タイプは、8 ビット符号なし整数値の配列
+
+- 0-3 NES(文字列)
+- 4 programROMPages
+- 5 characterROMPages
+- !(6 & 0x01) isHorizontalMirror
+- (((nes[6] & 0xF0) >> 4) | nes[7] & 0xF0) mapper
+
+- NES_HEADER_SIZE = 0x0010;
+const PROGRAM_ROM_SIZE = 0x4000;
+const CHARACTER_ROM_SIZE = 0x2000;
+
+
+JSR (Jump to new location saving return address)
+  ADDR -> PC
+  サブルーチンへジャンプします。
+  まずジャンプ先のアドレスをアドレス指定によって取得した後、 PCを上位バイト、下位バイトの順にスタックへプッシュします。 このときのPCはトラップの項にもあるようにJSRの最後のバイトアドレスです。 最後にジャンプ先へジャンプします。
+  flags: none
+
+RTS (Return from Subroutine)
+  サブルーチンから復帰します。
+  復帰アドレスをスタックから、下位バイト、 上位バイトの順にポップしたのちインクリメントします。
+  flags: none
--- src/index.js
+++ src/index.js
@@ -1,9 +1,51 @@
-const axios = require('axios')
+fetch('./static/hello.nes')
+  .then((res) => res.arrayBuffer())
+  .then((file) => {
+    console.log(file)
+  })
+  .catch((err) => {
+    throw new Error(err)
+  })
 
-axios.get('./static/hello.nes')
-      .then((ret) => {
-        console.log(ret)
-      })
-      .catch((err) => {
-        throw new Error(err)
-      })
+/**
+ * A アキュムレータ
+ * M アドレッシングによってフェッチされたメモリデータ
+ * flags 命令実行によって設定するフラグ
+*/
+const HelloOpecodes = [
+  'SEI', // Set Interrupt disable
+  'SEC', // Set C flag	1 -> C
+
+  'LDA', // Load A from M	  M -> A
+  'LDX', // Load X from M	  M -> X
+  'LDY', //
+
+  'TXS', // Transfer X to S	  X -> S
+  'TYA', // Transfer Y to A	  Y -> A
+
+  'STA', // Store A to M	  A -> M
+  'STX', // Store X to M
+
+  'BNE', // Branch on Z clear (result not equal)  Zフラグがクリアされていれば分岐します
+  'BPL', // Branch on N clear (result plus)
+  'BCS', // Branch on C set
+  'BEQ', // Branch on Z set (result equal)
+  'BCC', // Branch on C clear
+
+  'ADC', // Add M to A with C	    A + M + C -> A
+  'DEC', // Decrement M by one	  M - 1 -> M
+  'DEY', // Decrement Y by one    Y - 1 -> Y
+  'INC', // Increment M by one	M + 1 -> M
+
+  'RTS', // Return from Subroutine サブルーチンから復帰
+
+  'EOR', // EOR ("Exclusive-OR" M with A)	A eor M -> A
+
+  'JMP', // Jump to new location ADDR -> PC flags: none
+  'JSR', // Jump to new location saving return address  ADDR -> PC *
+
+  'CMP', // Compare M and A	    A - M
+
+  'TAY', // Transfer A to Y	    A -> Y
+  'CLC', // Clear C flag
+]
