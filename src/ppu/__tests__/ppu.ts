7cec487554aa117837cc3c1556a0a2bf37f1a7f2
rchaser53 <tayoshizawa29@gmail.com>
2018-04-06 23:48:13 +0900

    add getNameSpace test

--- src/ppu/__tests__/ppu.ts
+++ src/ppu/__tests__/ppu.ts
@@ -26,4 +26,33 @@ describe('PPU', () => {
     })
   })
 
+	describe('getNameSpace', () => {
+    test('false, false is 0x2000', async () => {
+      ppu.register.PPUCTRL.nameTableUpper = false
+      ppu.register.PPUCTRL.nameTableLowwer = false
+      const expected = 0x2000
+      expect(ppu.getNameSpace()).toEqual(expected)
+    })
+
+    test('false, true is 0x2400', async () => {
+      ppu.register.PPUCTRL.nameTableUpper = false
+      ppu.register.PPUCTRL.nameTableLowwer = true
+      const expected = 0x2400
+      expect(ppu.getNameSpace()).toEqual(expected)
+    })
+
+    test('true, false is 0x2800', async () => {
+      ppu.register.PPUCTRL.nameTableUpper = true
+      ppu.register.PPUCTRL.nameTableLowwer = false
+      const expected = 0x2800
+      expect(ppu.getNameSpace()).toEqual(expected)
+    })
+
+    test('true, true is 0x2C00', async () => {
+      ppu.register.PPUCTRL.nameTableUpper = true
+      ppu.register.PPUCTRL.nameTableLowwer = true
+      const expected = 0x2C00
+      expect(ppu.getNameSpace()).toEqual(expected)
+    })
+  })
 })
