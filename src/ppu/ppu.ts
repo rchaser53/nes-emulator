6d1064b7ce5dc7327764c34247cbb3bf4d803c7f
rchaser53 <tayoshizawa29@gmail.com>
2018-04-24 00:33:20 +0900

    rebuild ppu

--- src/ppu/ppu.ts
+++ src/ppu/ppu.ts
@@ -1,6 +1,8 @@
 import {
   convertDecimalToBoolArray,
-  createColorTileDef
+  convertIndexToRowColumn,
+  createColorTileDef,
+  createSpliteInputs
 } from './util';
 
 // 0x2000 - 0x2007
@@ -64,8 +66,9 @@ const ControlRegisterMap = {
   7: 'nameTableLowwer',
 }
 
-const BoarderCycle = 341;
-const DrawLine = 240;
+const LineLimit = 262;
+// const BoarderCycle = 341;
+// const DrawLine = 240;
 // const SpriteNumber = 100;
 
 const DefaultPPUCTRL: ControlRegister = {
@@ -95,8 +98,6 @@ export class PPU {
   line: number = 0
   background: any[] = []
   tileY: number
-	vram: Uint8Array
-	spriteRam: Uint8Array
 	sprites: any[] = []
   spriteRamAddr: number = 0
   vRamAddr: number = 0
@@ -104,6 +105,8 @@ export class PPU {
   isVramAddrUpper: boolean = true
 
   characteSpliteData: number[][]
+  spriteRam: Uint8Array = new Uint8Array(0x100)
+
   patternTable0: Uint8Array = new Uint8Array(0x1000)        //  $0000～$0FFF
   patternTable1: Uint8Array = new Uint8Array(0x1000)        //  $1000～$1FFF
   nameTable0: Uint8Array = new Uint8Array(0x3c0)            //  $2000～$23BF
@@ -120,7 +123,10 @@ export class PPU {
   splitePallete: Uint8Array = new Uint8Array(0x10)          //  $3F10～$3F1F
   palleteMirror: Uint8Array = new Uint8Array(0xD0)          //  $3F20～$3FFF
 
-  colorTileDefs: number[][] = []
+  colorTileDefs0: number[][] = []
+  colorTileDefs1: number[][] = []
+  colorTileDefs2: number[][] = []
+  colorTileDefs3: number[][] = []
 
   register: PPURegister = {
     PPUCTRL: DefaultPPUCTRL,
@@ -142,26 +148,6 @@ export class PPU {
     this.characteSpliteData = splites
   }
 
-  getPalette(): any {
-
-  }
-
-  getSpriteId(tileX, tileY): number {
-		return 0
-  }
-
-  getBlockId(tileX, tileY): number {
-		return 0
-  }
-
-  getAttribute(tileX, tileY): number {
-		return 0
-  }
-
-  readCharacterRAM(addr: number): number {
-		return 0
-  }
-
   read(index: number): number {
     switch (PPURegisterMap[index]) {
       case 'PPUSTATUS':
@@ -178,7 +164,6 @@ export class PPU {
   }
 
   write(index: number, value: number) {
-    console.log(index, value, 'nya-n', this)
     switch (PPURegisterMap[index]) {
       case 'PPUCTRL':
         this.writeBoolArrayRegister(value);
@@ -283,95 +268,38 @@ export class PPU {
 
   run(cycle: number){
     this.cycle += cycle;
-    if(this.line === 0) {
-      this.background.length = 0;
-      this.buildSprites();
-    }
+    this.line++
 
-    if (BoarderCycle <= this.cycle) {
-      this.cycle -= BoarderCycle;
-      this.line++;
-
-      if (this.line <= DrawLine && this.line % 8 === 0) {
-        this.buildBackground();
-      }
-
-      if (this.line === 262) {
-        this.line = 0;
-        return {
-          background: this.background,
-          palette: this.getPalette(),
-        };
-      }
+    if (this.line === LineLimit) {
+      this.colorTileDefs0 = createColorTileDef(this.attributeTable0 as any)
+      this.line = 0;
+      return {
+        background: this.buildBackground(),
+      };
     }
     return
   }
 
-	buildSprites() {
-		// TODO
-  }
-
-  buildSprite(spriteId) {
-    const sprite = new Array(8).fill(0).map(() => [0, 0, 0, 0, 0, 0, 0, 0]);
-    for (let i = 0; i < 16; i = i + 1) {
-      for (let j = 0; j < 8; j = j + 1) {
-        const addr = spriteId * 16 + i;
-        const ram = this.readCharacterRAM(addr);
-        if (ram & (0x80 >> j)) {
-          sprite[i % 8][j] += 0x01 << ~~(i / 8);
-        }
-      }
-    }
-    return sprite;
-  }
-
-  buildTile(tileX, tileY) {
-    const blockId = this.getBlockId(tileX, tileY); 
-    const spriteId = this.getSpriteId(tileX, tileY); 
-    const attr = this.getAttribute(tileX, tileY); 
-    const paletteId = (attr >> (blockId * 2)) & 0x03; 
-    const sprite = this.buildSprite(spriteId); 
-    return { 
-      sprite, 
-      paletteId, 
-    }; 
-  } 
 
   buildBackground() { 
-    const clampedTileY = this.tileY % 30; 
-    for (let x = 0; x < 32; x = x + 1) {
-      const clampedTileX = x % 32;
-      // const nameTableId = (~~(x / 32) % 2);
-      const tile = this.buildTile(clampedTileX, clampedTileY);
-      this.background.push(tile);
-    }
+    const names = Array.from(this.nameTable0)
+    return names.map((elem, index) => {
+      return this.createColorSplite(elem, index);
+    })
+  }
+
   createColorSplite(characterIndex: number, nameIndex: number) {
     const { row, column } = convertIndexToRowColumn(nameIndex)
-    return this.characteSpliteData[characterIndex].map((elem) => {
-      return this.colorTileDefs0[row][column] << 2 | elem;
+    const splite = createSpliteInputs(this.characteSpliteData[characterIndex]);
+
+    return splite.map((elem) => {
+      return elem.map((num) => {
+        const palleteIndex = this.colorTileDefs0[row][column] << 2 | num
+        return this.backgroundPallete[palleteIndex]
+      })
     })
   }
 }
-
-
-// BG（Back Ground）は8×8のタイルパターンを32×30個配置することで、 256×240ピクセルの解像度を持ちます。 
-
-// BG描画は、まずスキャン座標と設定されたスクロール値によって算出された座標を、
-// 範囲内に持つネームテーブルから描画するパターンのアドレスIDをフェッチします。 
-/* 15 */ 
-
-// また、色情報として属性テーブルからデータをフェッチします。
-/* 15 */ 
-
-// 次にパターンアドレスIDに対応する下位パターンと上位パターンをフェッチします。
-// パターンは上位ビットから描画され、 下位パターンと上位パターンの各ビットが有効なら、
-// 色情報とパターンビットによりBGパレットから色が選択されます。
-
-// パターンのいずれのビットも有効でなければ透過色となり、 スプライトパレットの$3F10の色が選択されます。
-
-const addressId = 72 //スキャン座標と設定されたスクロール値によって算出された座標
-
-
 // Initial Register Values
 // Register	                          At Power	              After Reset
 // PPUCTRL ($2000)	                    0000 0000	              0000 0000
